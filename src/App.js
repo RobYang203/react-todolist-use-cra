ff6aaaba378aa95dac3ebf1ecf692208aa7b9c41
tony <tonyyang@futengcom.com>
2021-02-28 21:17:31 +0800

    建立 todo list 邏輯

--- src/App.js
+++ src/App.js
@@ -1,31 +1,79 @@
-import { Component } from "react";
-import logo from "./logo.svg";
+import { Component, createRef } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.inputRef = createRef();
+    this.state = {
+      todoList: [],
+    };
+  }
+
+  createTodoItem = () => {
+    const { value } = this.inputRef.current;
+    if (/^\s*$/.test(value)) {
+      alert("不得為空值");
+      return;
+    }
+
+    const { todoList } = this.state;
+
+    const newTodoList = todoList.concat({
+      text: value,
+      createDate: new Date(),
+    });
+
+    this.setState({
+      todoList: newTodoList,
+    });
+
+    this.inputRef.current.value = "";
+  };
+
+  deleteTodoItem = (e, targetItem) => {
+    const { todoList } = this.state;
+    const index = todoList.findIndex((item) => {
+      return targetItem === item;
+    });
+    if (index === -1) {
+      console.log(index);
+      return;
+    }
+    todoList.splice(index, 1);
+    this.setState({
+      todoList,
+    });
+  };
+
+  buildTodoListComponent = (list = []) => {
+    return list.map((item) => {
+      const { text, createDate } = item;
+      return (
+        <li className="todo-item" key={createDate}>
+          <span>{text}</span>
+          <button
+            className="todo-item-delete"
+            onClick={(e) => this.deleteTodoItem(e, item)}
+          >
+            X
+          </button>
+        </li>
+      );
+    });
+  };
 
   render() {
     return (
-      <div >
-        <header className="App-header">
-          <input/>
-          <button>save</button>
+      <div>
+        <header className="todo-header">
+          <input ref={this.inputRef} />
+          <button onClick={this.createTodoItem}>save</button>
         </header>
-        <p className="todo-list">
-          <hr/>
-          <ul>
-            <li className="todo-item">
-              <span>todo 1</span>
-              <button className="todo-item-delete" >X</button>
-            </li>
-            <li>
-              todo 2
-            </li>
-            <li>
-              todo 3
-            </li>
-          </ul>
-        </p>
+        <div className="todo-list">
+          <hr></hr>
+          <ul>{this.buildTodoListComponent(this.state.todoList)}</ul>
+        </div>
       </div>
     );
   }
